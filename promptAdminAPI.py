da88517c992fafac1f5e0df24afbe7740b2c204e
David Shepard <adm.dshepard@transmission1.byu.edu>
2017-06-09 16:16:35 -0600

    added in try and catch to all web calls for error handling

--- promptAdminAPI.py
+++ promptAdminAPI.py
@@ -76,243 +76,272 @@ def establishDBConnection():
 
 @app.route("/getPrompts/<string:appId>")
 def getPrompts(appId):
-    #start connection to Oracle database
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
-    #return PIN from database that matches the entered app id
-    cur.execute('select AUTH_PIN from APPID_APPNAME where APP_ID ='+appId)
 
-    #grabs pin form sql query result
-    pin = ""
-    for item in cur:
-        pin = str(item)
-    pin = stringCleaner(pin)
+    try:
+        #start connection to Oracle database
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
+        #return PIN from database that matches the entered app id
+        cur.execute('select AUTH_PIN from APPID_APPNAME where APP_ID ='+appId)
 
-    #query to grab all prompt ids associated with the app id
-    cur.execute('select PROMPT_ID from APPID_PROMPTID where APP_ID ='+appId)
-    prompts = []
-    for member in cur:
-        prompts.append(str(member))
-    #close oracle connections
-    cur.close()
-    con.close()
-    prompts = listCleaner(prompts)
+        #grabs pin form sql query result
+        pin = ""
+        for item in cur:
+            pin = str(item)
+        pin = stringCleaner(pin)
 
-    #generate xml file
-    promptsXml = generateListXML(prompts,"prompts","prompt")
-    appIdXml = generateSingleXML(appId,"appID")
-    pinXml = generateSingleXML(pin,"pin")
-    responseXml = "<getPrompts>\n" + appIdXml + "\n" + pinXml + "\n" + promptsXml + "\n</getprompts>"
-    return responseXml
+        #query to grab all prompt ids associated with the app id
+        cur.execute('select PROMPT_ID from APPID_PROMPTID where APP_ID ='+appId)
+        prompts = []
+        for member in cur:
+            prompts.append(str(member))
+        #close oracle connections
+        cur.close()
+        con.close()
+        prompts = listCleaner(prompts)
+
+        #generate xml file
+        promptsXml = generateListXML(prompts,"prompts","prompt")
+        appIdXml = generateSingleXML(appId,"appID")
+        pinXml = generateSingleXML(pin,"pin")
+        responseXml = "<getPrompts>\n" + appIdXml + "\n" + pinXml + "\n" + promptsXml + "\n</getprompts>"
+        return responseXml
+    except:
+        return generateSingleXML("failure","response")
 
 @app.route("/changePin/<string:appId>/<string:newPin>")
 def changePin(appId,newPin):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to set pin
-    query = 'UPDATE APPID_APPNAME SET AUTH_PIN = '+newPin+' WHERE APP_ID ='+appId
-    cur.execute(query)
-    con.commit()
+        #query to set pin
+        query = 'UPDATE APPID_APPNAME SET AUTH_PIN = '+newPin+' WHERE APP_ID ='+appId
+        cur.execute(query)
+        con.commit()
 
-    #close oracle connections
-    cur.close()
-    con.close()
+        #close oracle connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
-
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 
 @app.route("/addPrompt/<string:appId>/<string:promptId>/<string:promptName>")
 def addPrompt(appId,promptId,promptName):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to add new prompt
-    named_params = {'app':appId, 'prompt':promptId, 'name':promptName}
-    query = 'INSERT INTO APPID_PROMPTID (APP_ID,PROMPT_ID,DESCRIPTION) VALUES (:app, :prompt, :name)'
-    cur.execute(query, named_params)
-    con.commit()
+        #query to add new prompt
+        named_params = {'app':appId, 'prompt':promptId, 'name':promptName}
+        query = 'INSERT INTO APPID_PROMPTID (APP_ID,PROMPT_ID,DESCRIPTION) VALUES (:app, :prompt, :name)'
+        cur.execute(query, named_params)
+        con.commit()
 
-    #close connections
-    cur.close()
-    con.close()
+        #close connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
-
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure", "response")
 
 @app.route("/changeGroupName/<string:appId>/<string:groupName>")
 def changeGroupName(appId,groupName):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to change group name
-    named_params = {'newName':groupName}
-    query = 'UPDATE APPID_APPNAME SET APP_NAME =:newName WHERE APP_ID ='+appId
-    cur.execute(query,named_params)
-    con.commit()
+        #query to change group name
+        named_params = {'newName':groupName}
+        query = 'UPDATE APPID_APPNAME SET APP_NAME =:newName WHERE APP_ID ='+appId
+        cur.execute(query,named_params)
+        con.commit()
 
-    #close oracle connections
-    cur.close()
-    con.close()
+        #close oracle connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 @app.route("/setFc/<string:appId>/<string:boolean>")
 def setFc(appId,boolean):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to update fc
-    named_params = {'boolVal':boolean}
-    query = 'UPDATE FCFO_STATUS SET FORCE_CLOSE =:boolVal WHERE APP_ID = '+appId
-    cur.execute(query,named_params)
-    con.commit()
+        #query to update fc
+        named_params = {'boolVal':boolean}
+        query = 'UPDATE FCFO_STATUS SET FORCE_CLOSE =:boolVal WHERE APP_ID = '+appId
+        cur.execute(query,named_params)
+        con.commit()
 
-    #close oracle connections
-    cur.close()
-    con.close()
+        #close oracle connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 @app.route("/setFo/<string:appId>/<string:boolean>")
 def setFo(appId,boolean):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to update fc
-    named_params = {'boolVal':boolean}
-    query = 'UPDATE FCFO_STATUS SET FORCE_OPEN =:boolVal WHERE APP_ID = '+appId
-    cur.execute(query,named_params)
-    con.commit()
+        #query to update fc
+        named_params = {'boolVal':boolean}
+        query = 'UPDATE FCFO_STATUS SET FORCE_OPEN =:boolVal WHERE APP_ID = '+appId
+        cur.execute(query,named_params)
+        con.commit()
 
-    #close oracle connections
-    cur.close()
-    con.close()
+        #close oracle connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("success","response")
 
 
 
 @app.route("/addApp/<string:appId>/<string:groupName>/<string:promptId>/<string:promptName>/<string:pin>")
 def addApp(appId,groupName,promptId,promptName,pin):
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to create new user
-    named_params = {'app':appId, 'group':groupName, 'promptIdent':promptId, 'prompt':promptName, 'pinNumber':pin, 'boolVal':'false'}
-    query = 'INSERT INTO APPID_APPNAME (APP_ID, APP_NAME,AUTH_PIN) VALUES (:app,:group,:pinNumber)'
-    cur.execute(query,named_params)
-    con.commit()
-    query = 'INSERT INTO APPID_PROMPTID (APP_ID, PROMPT_ID, DESCRIPTION) VALUES (:app,:promptIdent,:prompt)'
-    cur.execute(query,named_params)
-    con.commit()
-    query = 'INSERT INTO FCFO_STATUS (APP_ID, FORCE_CLOSE, FORCE_OPEN) VALUES (:app,:boolVal,:boolVal)'
-    con.execute(query,named_params)
-    con.commit()
+        #query to create new user
+        named_params = {'app':appId, 'group':groupName, 'promptIdent':promptId, 'prompt':promptName, 'pinNumber':pin, 'boolVal':'false'}
+        query = 'INSERT INTO APPID_APPNAME (APP_ID, APP_NAME,AUTH_PIN) VALUES (:app,:group,:pinNumber)'
+        cur.execute(query,named_params)
+        con.commit()
+        query = 'INSERT INTO APPID_PROMPTID (APP_ID, PROMPT_ID, DESCRIPTION) VALUES (:app,:promptIdent,:prompt)'
+        cur.execute(query,named_params)
+        con.commit()
+        query = 'INSERT INTO FCFO_STATUS (APP_ID, FORCE_CLOSE, FORCE_OPEN) VALUES (:app,:boolVal,:boolVal)'
+        con.execute(query,named_params)
+        con.commit()
 
-    #close connections
-    cur.close()
-    con.close()
+        #close connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 
 @app.route("/returnAll")
 def returnAll():
-    #start oracle connection
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #start oracle connection
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to natural join FCFO_STATUS and APPID_APPNAME
-    query = ('SELECT * FROM APPID_APPNAME NATURAL JOIN FCFO_STATUS')
-    cur.execute(query)
-    applications = []
-    for app in cur:
-        temp = str(app).split(",")
-        temp =  listCleaner(temp)
-        applications.append(temp)
+        #query to natural join FCFO_STATUS and APPID_APPNAME
+        query = ('SELECT * FROM APPID_APPNAME NATURAL JOIN FCFO_STATUS')
+        cur.execute(query)
+        applications = []
+        for app in cur:
+            temp = str(app).split(",")
+            temp =  listCleaner(temp)
+            applications.append(temp)
 
-    #query to retrieve prompts for each app
-    appDataXml = generateNestedListXML(applications)
-    promptDataXml = []
-    for app in applications:
-        promptQuery = 'SELECT PROMPT_ID, DESCRIPTION FROM APPID_PROMPTID WHERE APP_ID = '+app[0]
-        cur.execute(promptQuery)
-        promptXml = "<prompts>\n"
-        for item in cur:
-            temp = str(item).split(",")
-            temp = listCleaner(temp)
-            promptXml = promptXml + "\t<prompt>\n\t\t<promptid>"+temp[0]+"</promptid>\n\t\t<promptname>"+temp[1]+"</promptname>\n\t</prompt>\n"
-        promptXml = promptXml + "</prompts>\n"
-        promptDataXml.append(promptXml)
+        #query to retrieve prompts for each app
+        appDataXml = generateNestedListXML(applications)
+        promptDataXml = []
+        for app in applications:
+            promptQuery = 'SELECT PROMPT_ID, DESCRIPTION FROM APPID_PROMPTID WHERE APP_ID = '+app[0]
+            cur.execute(promptQuery)
+            promptXml = "<prompts>\n"
+            for item in cur:
+                temp = str(item).split(",")
+                temp = listCleaner(temp)
+                promptXml = promptXml + "\t<prompt>\n\t\t<promptid>"+temp[0]+"</promptid>\n\t\t<promptname>"+temp[1]+"</promptname>\n\t</prompt>\n"
+            promptXml = promptXml + "</prompts>\n"
+            promptDataXml.append(promptXml)
 
-    #merge xml for app and prompt data
-    combinedXml = "<data>\n"
-    for app, prompt in zip(appDataXml,promptDataXml):
-        combinedXml = combinedXml +"<appobject>\n"+ app + prompt +"</appobject>\n"
-    combinedXml = combinedXml + "</data>\n"
+        #merge xml for app and prompt data
+        combinedXml = "<data>\n"
+        for app, prompt in zip(appDataXml,promptDataXml):
+            combinedXml = combinedXml +"<appobject>\n"+ app + prompt +"</appobject>\n"
+        combinedXml = combinedXml + "</data>\n"
 
 
-    #close connections
-    cur.close()
-    con.close()
+        #close connections
+        cur.close()
+        con.close()
 
-    return combinedXml
+        return combinedXml
+    except:
+        return generateSingleXML("failure","response")
 
 
 
 @app.route("/deletePrompt/<string:promptId>")
 def promptId(promptId):
-    #connect to oracle
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #connect to oracle
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #query to delete prompt
-    named_params = {'id':promptId}
-    query = 'DELETE FROM APPID_PROMPTID WHERE PROMPT_ID =:id'
-    cur.execute(query,named_params)
-    con.commit()
+        #query to delete prompt
+        named_params = {'id':promptId}
+        query = 'DELETE FROM APPID_PROMPTID WHERE PROMPT_ID =:id'
+        cur.execute(query,named_params)
+        con.commit()
 
 
-    #end connections
-    cur.close()
-    con.close()
-    return generateSingleXML("success","response")
+        #end connections
+        cur.close()
+        con.close()
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 
 
 
 @app.route("/deleteApp/<string:appId>")
 def deleteApp(appId):
-    #connect to oracle
-    con = cx_Oracle.connect(establishDBConnection())
-    cur = con.cursor()
+    try:
+        #connect to oracle
+        con = cx_Oracle.connect(establishDBConnection())
+        cur = con.cursor()
 
-    #queries to delete
-    named_params = {'app':appId}
-    query = 'DELETE FROM APPID_APPNAME WHERE APP_ID =:app'
-    cur.execute(query, named_params)
+        #queries to delete
+        named_params = {'app':appId}
+        query = 'DELETE FROM APPID_APPNAME WHERE APP_ID =:app'
+        cur.execute(query, named_params)
 
-    query = 'DELETE FROM FCFO_STATUS WHERE APP_ID =:app'
-    cur.execute(query,named_params)
+        query = 'DELETE FROM FCFO_STATUS WHERE APP_ID =:app'
+        cur.execute(query,named_params)
 
-    query = 'DELETE FROM APPID_PROMPTID WHERE APP_ID =:app'
-    cur.execute(query,named_params)
-    con.commit()
+        query = 'DELETE FROM APPID_PROMPTID WHERE APP_ID =:app'
+        cur.execute(query,named_params)
+        con.commit()
 
 
-    #end connections
-    cur.close()
-    con.close()
+        #end connections
+        cur.close()
+        con.close()
 
-    return generateSingleXML("success","response")
+        return generateSingleXML("success","response")
+    except:
+        return generateSingleXML("failure","response")
 
 
 
